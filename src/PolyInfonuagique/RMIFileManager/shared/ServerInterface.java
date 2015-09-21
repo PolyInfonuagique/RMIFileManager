4af997043bf14988c15a4417094f8bad3004bb80
David Livet <david.livet@orange.fr>
2015-09-21 18:06:50 -0400

    Update ServerInterface

--- src/PolyInfonuagique/RMIFileManager/shared/ServerInterface.java
+++ src/PolyInfonuagique/RMIFileManager/shared/ServerInterface.java
@@ -1,11 +1,11 @@
 package PolyInfonuagique.RMIFileManager.shared;
 
 public interface ServerInterface {
-	void generateclientid();
-	void create(String nom);
-	String list();
-	void syncLocalDir();
-	Integer get(String name, String checksum);
-	Integer lock(String nom, Integer clientid, String checksum);
-	void push(String nom, byte[] contenu, Integer clientid);
+	public void generateClientId();
+	public void create(String nom);
+	public String list();
+	public void syncLocalDir();
+	public Integer get(String name, String checksum);
+	public Integer lock(String nom, Integer clientid, String checksum);
+	public void push(String nom, byte[] contenu, Integer clientid);
 }
